deff1ad6cc70afc3143de69a6c3b09c4cc56b8e1
Jenny Knuth <inkboco@gmail.com>
2015-06-03 13:23:38 -0600

    tried a couple

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ var lib = require('./lib.js');
 array = [1,2,3,4,5,6,7,18]
 
 console.log(lib);
-console.log(array.slice(3,4));
 
-console.log("add" + array.slice(3,4) + ": ", lib.add(array.slice(3,4));
-console.log("subtract 1 and 2: ", lib.subtract(array.slice(5, 3));
+console.log("add" + ": ", lib.add(3,5));
+console.log("subtract", lib.subtract(5,3));
