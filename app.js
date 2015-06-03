0bf7f9acc07f41a4ff076d135e5e46d800be8604
Jenny Knuth <inkboco@gmail.com>
2015-06-03 11:45:26 -0600

    starting to use

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
 var lib = require('./lib.js');
 
+array = [1,2,3,4,5,6,7,18]
+
 console.log(lib);
+console.log(array.slice(3,4));
+
+console.log("add" + array.slice(3,4) + ": ", lib.add(array.slice(3,4));
+console.log("subtract 1 and 2: ", lib.subtract(array.slice(5, 3));
